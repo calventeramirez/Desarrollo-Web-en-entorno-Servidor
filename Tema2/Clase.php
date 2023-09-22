8221de50f26daa6549bb8ac75b08d243a781cd41
Pablo Calvente <calventeramirez@outlook.com>
2023-09-22 14:42:42 +0200

    Añadido ejercicio tema 2

--- Tema2/Clase.php
+++ Tema2/Clase.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tema 2</title>
 </head>
 <body>
     <?php
@@ -12,10 +12,19 @@
             'key2' => 'value2',
             'key3' => 'value3',
         );
-        $array2 = array('value1', 'value2', 'value3';
+        $array2 = array('value1', 'value2', 'value3');
         print_r($array1);
         print("<br><br>");
         print_r($array2);
+        print("<br><br>");
+        //Crear el array con dni como clave y como valor el nombre
+        $arrayPersona = array(
+            "53899207H" => "Pablo Calvente",
+            "58602514J" => "Pepe Gonzalez",
+            "58605854L" => "Manolo Gonzalez",
+            "78612508V" => "Mario Perez",
+        );
+        print_r($arrayPersona);
         print("<br><br>");
     ?>
 </body>
