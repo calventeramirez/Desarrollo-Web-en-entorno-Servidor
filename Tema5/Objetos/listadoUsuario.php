ff0cdf41575c976698f02f2c04c43e27f8cacedf
Pablo Calvente <calventeramirez@outlook.com>
2023-10-30 10:48:21 +0100

    añadido script de bootstrap

--- Tema5/Objetos/listadoUsuario.php
+++ Tema5/Objetos/listadoUsuario.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,42 +9,45 @@
     <?php require '../Funciones/base_de_datos.php' ?>
     <?php require 'usuario.php' ?>
 </head>
+
 <body>
-    <div class = "container">
+    <div class="container">
         <h1>Listado de Usuarios</h1>
         <div>
             <?php
-                 $sql = "SELECT * FROM usuarios";
-                 $resultado = $conexion -> query($sql);
-                 $usuarios = [];
-                 while($fila = $resultado ->fetch_assoc()){
-                     $nuevo_usuario = new Usuario($fila["usuario"], $fila["nombre"], $fila["apellidos"], $fila["fecha_nacimiento"]);
-                     array_push($usuarios, $nuevo_usuario);
-                 }
+            $sql = "SELECT * FROM usuarios";
+            $resultado = $conexion->query($sql);
+            $usuarios = [];
+            while ($fila = $resultado->fetch_assoc()) {
+                $nuevo_usuario = new Usuario($fila["usuario"], $fila["nombre"], $fila["apellidos"], $fila["fecha_nacimiento"]);
+                array_push($usuarios, $nuevo_usuario);
+            }
             ?>
             <table class="table table-hover table-striped table-responsive">
-                 <thead class = "table table-dark">
+                <thead class="table table-dark">
                     <tr>
                         <th>Usuario</th>
                         <th>Nombre</th>
                         <th>Apellidos</th>
                         <th>Fecha de Nacimiento</th>
                     </tr>
-                 </thead>
-                 <tbody>
-                 <?php
-                        foreach ($usuarios as $usuario) {
-                            echo "<tr>";
-                            echo "<td>" . $usuario->usuario . "</td>";
-                            echo "<td>" . $usuario->nombre . "</td>";
-                            echo "<td>" . $usuario->apellidos . "</td>";
-                            echo "<td>" . $usuario->fecha_nacimiento . "</td>";
-                            echo "</tr>";
-                        }
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($usuarios as $usuario) {
+                        echo "<tr>";
+                        echo "<td>" . $usuario->usuario . "</td>";
+                        echo "<td>" . $usuario->nombre . "</td>";
+                        echo "<td>" . $usuario->apellidos . "</td>";
+                        echo "<td>" . $usuario->fecha_nacimiento . "</td>";
+                        echo "</tr>";
+                    }
                     ?>
-                 </tbody>
+                </tbody>
             </table>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
