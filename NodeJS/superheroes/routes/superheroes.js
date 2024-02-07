ccc79a84b0b711a9b0b6701a3e7e91ce39f16138
Pablo Calvente <calventeramirez@outlook.com>
2024-02-07 11:18:39 +0100

    Añadido la parte superheroe desde mongo

--- NodeJS/superheroes/routes/superheroes.js
+++ NodeJS/superheroes/routes/superheroes.js
@@ -1,17 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+const database = client.db("superheroes");
+const superpoderesCollection = database.collection("superpoderes");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    let arraySuperheroes = [
-        "Superman",
-        "Mr Increibles",
-        "Flash",
-        "Homelander",
-        "Viuda Negra"
-    ];
-
-  res.render('superheroes/index', { title: 'Superheroes', superheroes: arraySuperheroes });
+router.get('/', async (req, res, next) => {
+    // let arraySuperheroes = [
+    //     "Superman",
+    //     "Mr Increibles",
+    //     "Flash",
+    //     "Homelander",
+    //     "Viuda Negra"
+    // ];
+    let superpoderes = getAllSuperpoderes();
+  res.render('superheroes/index', { title: 'Superheroes', superheroes: await superpoderes });
 });
+
+async function getAllSuperpoderes(){
+  try{
+
+    let query = {};
+    let superpoderes = await superpoderesCollection.find(query).toArray();
+    console.log(superpoderes);
+    return superpoderes;
+  }finally{
+    //Esto es una guarrada, deberiamos hacer un catch
+  }
+}
+
+module.exports = router;
 
 module.exports = router;
